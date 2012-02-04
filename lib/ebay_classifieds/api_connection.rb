019b92e3e46d94cc72f416de94eb49b28b2d1624
Demersus (Nik Petersen) <demersus@gmail.com>
2012-02-04 01:15:21 -0800

    Expanded api_connection to wrap all http methods with fresh settings to api username, password, and url

--- lib/ebay_classifieds/api_connection.rb
+++ lib/ebay_classifieds/api_connection.rb
@@ -1,9 +1,28 @@
 require 'httparty'
 class EbayClassifieds::ApiConnection
   include HTTParty
-  def self.get(*args)
+  def self.setup_defaults
     digest_auth EbayClassifieds.api_username, EbayClassifieds.api_password
     base_uri EbayClassifieds.api_url
+  end
+  def self.get(*args)
+    setup_defaults
+    super(*args)
+  end
+  def self.head(*args)
+    setup_defaults
+    super(*args)
+  end
+  def self.put(*args)
+    setup_defaults
+    super(*args)
+  end
+  def self.post(*args)
+    setup_defaults
+    super(*args)
+  end
+  def self.delete(*args)
+    setup_defaults
     super(*args)
   end
 end
