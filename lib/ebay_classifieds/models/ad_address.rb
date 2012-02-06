23279e5165d4b98dd0f373eb759a56aae0128541
Demersus (Nik Petersen) <demersus@gmail.com>
2012-02-06 13:00:03 -0800

    Fixed the ad_address.  It wasn't correctly initializing it's attributes.

--- lib/ebay_classifieds/models/ad_address.rb
+++ lib/ebay_classifieds/models/ad_address.rb
@@ -1,7 +1,6 @@
 module EbayClassifieds
   module Models
-    class AdAddress 
-      attr_accessor(:street,:city,:state,:zip_code,:longitude,:latitude)
+    class AdAddress < Struct.new(:street,:city,:state,:zip_code,:longitude,:latitude)
     
       def self.new_from_api_data(data)
         new(data)
